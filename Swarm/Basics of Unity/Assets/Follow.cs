c9e34d2259145a200d8062f2a58ebf276f2b5852
PulkitDhamija <pewzcode@gmail.com>
2021-07-30 00:14:45 +0530

    Swarm Robotics Evaluation

---
+++ Swarm/Basics of Unity/Assets/Follow.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    // Start is called before the first frame update
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.position + offset;
+    }
+}
